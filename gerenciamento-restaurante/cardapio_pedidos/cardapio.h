181b43d68e0753525aa8f834681739582d5b1ffd
whorshe <whorshes@gmail.com>
2025-03-14 12:38:45 -0300

    Update cardapio.h

--- gerenciamento-restaurante/cardapio_pedidos/cardapio.h
+++ gerenciamento-restaurante/cardapio_pedidos/cardapio.h
@@ -4,28 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h> // Incluído caso você precise de suporte a localização
 
+// Enumeração de categorias do cardápio
 typedef enum {entrada, principal, sobremesa, bebida} categoria;
 
 // Estrutura para representar um item do cardápio
 typedef struct {
-    char nome[100]; // Nome do item
-    char descri[100]; // Descrição do item
-    float preco; // Preço do item
+    char nome[100]; // nome do item
+    char descri[100]; // descrição do item
+    float preco; // preço em R$ do item
     categoria catego; // Categoria do item
 } item;
 
-// Função para exibir o cardápio
+// Funções para manipulação do cardápio
 void exibir_card(item* cardapio, int codigo);
-
-// Função para cadastrar um item no cardápio
 void cadastro_card(item *p);
-
-// Função para remover um item do cardápio
+void redimensionar_cardapio(item** cardapio, int* capacidade_cardapio);
 void remover_card(item* cardapio, int* codigo);
 
-// Função para redimensionar o cardápio
-void redimensionar_cardapio(item** cardapio, int* capacidade_cardapio);
+extern char* categs[]; // Declaração externa para o array categs
 
 #endif // CARDAPIO_H
