3e241657b919924ae90acdfeda9bcc6363431369
whorshe <whorshes@gmail.com>
2025-03-14 13:02:04 -0300

    Update utilitarios.c

--- gerenciamento-restaurante/cardapio_pedidos/utilitarios.c
+++ gerenciamento-restaurante/cardapio_pedidos/utilitarios.c
@@ -1,5 +1,7 @@
-#include "utilitarios.h"
+#include <stdio.h>      // Para funções de entrada e saída
+#include <stdlib.h>     // Para funções de alocação de memória (se necessário)
+#include "utilitarios.h" // Para a declaração da função limpar_buffer
 
 void limpar_buffer() {
-    while(getchar() != '\n');
+    while(getchar() != '\n'); // Limpa o buffer até encontrar uma nova linha
 }
