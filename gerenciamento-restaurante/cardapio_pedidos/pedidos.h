aa285b2738579ef1c0b99629426ca3913324bab3
whorshe <whorshes@gmail.com>
2025-03-14 12:44:46 -0300

    Update pedidos.h

--- gerenciamento-restaurante/cardapio_pedidos/pedidos.h
+++ gerenciamento-restaurante/cardapio_pedidos/pedidos.h
@@ -1,21 +1,18 @@
 #ifndef PEDIDOS_H
 #define PEDIDOS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "cardapio.h" // Certifique-se de que este cabeçalho está correto
+#include "cardapio.h" // Inclua cardapio.h para usar a estrutura item
 
 // Enumeração para o status dos pedidos
 typedef enum {PENDENTE, EM_PREPARO, PRONTO, ENTREGUE} StatusPedido;
 
 // Estrutura para representar um pedido
 typedef struct {
-    int cod_pedido; // Código do pedido
-    char nome_cliente[100]; // Nome do cliente
-    item* itens; // Ponteiro para os itens do pedido
-    int num_itens; // Número de itens no pedido
-    StatusPedido status; // Status do pedido
+    int cod_pedido;
+    char nome_cliente[100];
+    item* itens;
+    int num_itens;
+    StatusPedido status;
 } Pedido;
 
 // Funções para gerenciar pedidos
