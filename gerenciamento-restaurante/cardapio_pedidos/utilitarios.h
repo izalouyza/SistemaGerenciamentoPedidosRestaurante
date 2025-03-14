8ed42e5b4ce8c811dae501f6fad72c0f148ca095
whorshe <whorshes@gmail.com>
2025-03-14 12:51:37 -0300

    Update utilitarios.h

--- gerenciamento-restaurante/cardapio_pedidos/utilitarios.h
+++ gerenciamento-restaurante/cardapio_pedidos/utilitarios.h
@@ -1,17 +1,9 @@
 #ifndef UTILITARIOS_H
 #define UTILITARIOS_H
 
-#include "cardapio.h" // Inclua cardapio.h para usar a estrutura item
+#include <stdio.h>
+#include <stdlib.h>
 
-// Funções para manipulação do cardápio e pedidos
-void exibir_card(item* cardapio, int codigo);
-void cadastro_card(item *p);
-void redimensionar_cardapio(item** cardapio, int* capacidade_cardapio);
-void remover_card(item* cardapio, int* codigo);
-void criar_pedido_menu(item* cardapio, int codigo, Pedido** pedidos, int* num_pedidos, int* capacidade_pedidos);
-void gerenciar_pedidos(Pedido* pedidos, int num_pedidos);
-void alterar_status(Pedido* pedidos, int num_pedidos);
-void alterar_pedido(Pedido* pedidos, int num_pedidos, item* cardapio, int codigo);
-void remover_pedido(Pedido* pedidos, int* num_pedidos);
+void limpar_buffer();
 
 #endif // UTILITARIOS_H
