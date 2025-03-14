100dd1139d6086e7e687a9a5b05e9c4bf01d54be
whorshe <whorshes@gmail.com>
2025-03-14 12:44:58 -0300

    Update utilitarios.h

--- gerenciamento-restaurante/cardapio_pedidos/utilitarios.h
+++ gerenciamento-restaurante/cardapio_pedidos/utilitarios.h
@@ -1,34 +1,7 @@
 #ifndef UTILITARIOS_H
 #define UTILITARIOS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-
-// Enumeração de categorias do cardápio
-typedef enum {entrada, principal, sobremesa, bebida} categoria;
-extern char* categs[]; // Declaração externa para o array categs
-
-// Struct para representar um item do cardápio
-typedef struct {
-    char nome[100]; // Nome do item
-    char descri[100]; // Descrição do item
-    float preco; // Preço do item
-    categoria catego; // Categoria do item
-} item;
-
-// Enum para status dos pedidos
-typedef enum {PENDENTE, EM_PREPARO, PRONTO, ENTREGUE} StatusPedido;
-
-// Struct para representar um pedido
-typedef struct {
-    int cod_pedido; // Código do pedido
-    char nome_cliente[100]; // Nome do cliente
-    item* itens; // Ponteiro para os itens do pedido
-    int num_itens; // Número de itens no pedido
-    StatusPedido status; // Status do pedido
-} Pedido;
+#include "cardapio.h" // Inclua cardapio.h para usar a estrutura item
 
 // Funções para manipulação do cardápio e pedidos
 void exibir_card(item* cardapio, int codigo);
