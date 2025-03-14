bda3db2165efa3381a63c3b7a699f650b26017c8
whorshe <whorshes@gmail.com>
2025-03-14 17:05:09 -0300

    Update utilitarios.h

--- gerenciamento-restaurante/cardapio_pedidos/utilitarios.h
+++ gerenciamento-restaurante/cardapio_pedidos/utilitarios.h
@@ -1,7 +1,14 @@
 #ifndef UTILITARIOS_H
 #define UTILITARIOS_H
 
-// Função para limpar o buffer de entrada
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Funções utilitárias
 void limpar_buffer();
+int ler_inteiro(const char* mensagem);
+float ler_float(const char* mensagem);
+void formatar_string(char* str, size_t tamanho);
 
 #endif // UTILITARIOS_H
